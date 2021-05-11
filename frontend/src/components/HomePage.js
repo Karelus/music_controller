d8e6a0f128f9d7a6f5a5d8429b37acaa6f623807
Karel Haapasalo <karel.haapasalo@hotmail.com>
2021-05-11 11:10:24 +0300

    craeted getRoom view and room page

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
+import Room from './Room';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 
 export default class HomePage extends Component {
@@ -12,8 +13,9 @@ export default class HomePage extends Component {
         return (<Router>
             <Switch>
                 <Route exact path='/' component=''><p>This is the home page!</p></Route>
-                <Route path='/join' component={RoomJoinPage}></Route>
-                <Route path='/create' component={CreateRoomPage}></Route>
+                    <Route path='/join' component={RoomJoinPage}></Route>
+                    <Route path='/create' component={CreateRoomPage}></Route>
+                    <Route path='/room/:roomCode' component={Room}></Route>
             </Switch>
         </Router>);
     }
